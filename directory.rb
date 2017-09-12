ae82fd62e6a92b7ae8028708c2f5a16eec955349
Georgios <george.lamprakis@gmail.com>
2017-09-12 17:53:56 +0100

    Refactored directory.rb

--- directory.rb
+++ directory.rb
@@ -1,62 +1,63 @@
+@students=[]
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    
-    students = []
-    
     name = gets.chomp
-    
     while !name.empty? do
-   
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         # get another name from the user
         name = gets.chomp
     end
-    # return the array of students
-    students
 end
   
 def print_header
-    puts "The students of my cohort at Makers Academy"
+    puts "The students of Villains Academy"
     puts "-------------"
 end
 
-def print(students)
-    students.each do |student|
+def print_students_list
+    @students.each do |student|
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer names
-    puts "Overall, we have #{names.count} great students"
+def print_footer 
+    puts "Overall, we have #{@students.count} great students"
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" 
+end
+def show_students
+    if @students.size > 0
+        print_header
+        print_students_list
+        print_footer
+    else
+        puts "No students inputted!"
+    end
+end
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "9"
+        exit 
+    else
+        puts "I don't know what you meant, try again"
+    end
 end
 
 def interactive_menu
-    students = []
     loop do
-        
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9. Exit" 
+        print_menu
         selection = gets.chomp
-        
-        case selection
-            when "1"
-                students += input_students
-            when "2"
-                if students.size > 0
-                    print_header
-                    print(students)
-                    print_footer(students)
-                else
-                    puts "No students inputted!"
-                end
-            when "9"
-                exit 
-            else
-                puts "I don't know what you meant, try again"
-        end
+        process(selection)
     end
 end
 
