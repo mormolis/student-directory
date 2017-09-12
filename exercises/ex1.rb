6add361c5b4643b1a8940d93da7fc333259be496
Georgios <george.lamprakis@gmail.com>
2017-09-12 13:13:09 +0100

    Exercise 1 added

---
+++ exercises/ex1.rb
@@ -0,0 +1,34 @@
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "--------------------------------"
+end
+def print(names)
+    names.each_with_index do |name, i|
+        puts "#{i+1}. #{name[:name]} (#{name[:cohort]} cohort)"
+    end
+end
+#total number of students
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    #create an empty array
+    students=[]
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+end
+
+students = input_students
+
+#printing 
+print_header
+print(students)
+print_footer(students)
