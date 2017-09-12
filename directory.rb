ab3347193d3592e18fc259d5c40cdc39be5a7807
Georgios <george.lamprakis@gmail.com>
2017-09-12 12:34:10 +0100

    Array is being populated interactively

--- directory.rb
+++ directory.rb
@@ -1,17 +1,4 @@
-#array of students
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-  ]
+
 def print_header
     puts "The students of Villains Academy"
     puts "--------------------------------"
@@ -25,6 +12,21 @@ end
 def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    #create an empty array
+    students=[]
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+end
+
+students = input_students
 
 #printing 
 print_header
